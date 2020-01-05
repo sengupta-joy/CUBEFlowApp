4e131eec0a6ea402a9cf62c565c0fe636d62a8ad
joy.sengupta <joy.sengupta@live.com>
2020-01-05 20:48:11 +0300

    new

---
+++ CubeFlowApp/CubeFlowApp/Controllers/loginController.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class loginController
+        Inherits Controller
+
+        ' GET: login
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
