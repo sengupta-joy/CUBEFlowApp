38e3d5230b52e4a84f60b0f1c95909844571b840
joy.sengupta <joy.sengupta@live.com>
2020-01-20 21:45:30 +0300

    menu fixed

--- CubeFlowApp/CubeFlowApp/Scripts/layout.js
+++ CubeFlowApp/CubeFlowApp/Scripts/layout.js
@@ -27,8 +27,8 @@ function getCookie(name) {
     return null;
 }
 
-function getParentMenu(menuText) {
-    var str = "<li class='nav-item has-treeview'><a href = '#' class='nav-link' ><i class='nav-icon fas fa-edit'></i><p>" + menuText + "<i class='fas fa-angle-left right'></i></p></a ><ul class='nav nav - treeview subMenu'></ul></li >";
+function getParentMenu(menuText,ID) {
+    var str = "<li class='nav-item has-treeview rootMenu' tag='"+ ID +"'><a href = '#' class='nav-link' ><i class=''></i><p>" + menuText + "<i class='fas fa-angle-left right'></i></p></a ><ul class='nav nav-treeview subMenu'></ul></li >";
     return str;
 }
 
@@ -37,38 +37,56 @@ function getChildMenu(Name, Address) {
     return str;
 }
 getMenus = function () {
-    var url = getCookie("APIUrl")+"/menu";
+    var url = getCookie("APIUrl") + "/menu";
     
-    $.ajax({
-        url: url,
-        data: {},
-        type: 'GET',
-        dataType: 'json',
-        contentType: 'application/json; charset=utf-8',
-        beforeSend: function (xhr) { xhr.setRequestHeader('token', getCookie("APIKey")); },
-        success: function (result) {
-            x = (result);
-            $(".nav-pills.nav-sidebar").children().remove();
-            $(result).each(function (k, v) {
-                if (v.Address == "" && v.Parent == "") {
-                    var item = getParentMenu(v.Name);
-                    var menuItem = $(".nav-pills.nav-sidebar").append(item);
-                    var subMenu = $(result).filter(function () { return this.Parent == v.ID });
-                    if ($(subMenu).length > 0) {
-                        var childMenu = $($(item).children("ul.subMenu"));
-                        $(subMenu).each(function (i, j) {
-                            var childItem = getChildMenu(j.Name, j.Address);
-                            $(childMenu).append("<li>abcd</li>");
-                        });
-                    }
-                }
+    if (sessionStorage.getItem("menus") != null) {
+        loadMenu();
+        
+    } else {
+        $.ajax({
+            url: url,
+            data: {},
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            beforeSend: function (xhr) { xhr.setRequestHeader('token', getCookie("APIKey")); },
+            success: function (result) {
+                sessionStorage.setItem("menus", JSON.stringify(result));
+                loadMenu();
+            },
+            error: function (error) {
+           
+            }
+        });
+    }
+    
+   
+}
 
-
-            });
-        },
-        error: function (error) {
-            //console.log(error);
-            $("#lblMsg").text("invalid attempt");
+function loadMenu() {
+    var result = JSON.parse(sessionStorage.getItem("menus"));
+    $(".nav-pills.nav-sidebar").children().remove();
+    $(".nav-pills.nav-sidebar").append("<li class='nav-header'>Menus</li>");
+    $(result).filter(function () { return this.MenuType == 0 }).each(function (k, v) {
+        if (v.Address == "" && v.Parent == "") {
+            var item = getParentMenu(v.Name, v.ID);
+            var menuItem = $(".nav-pills.nav-sidebar").append(item);
         }
     });
+    $(".nav-pills.nav-sidebar").append("<li class='nav-header'>Plugins</li>");
+    $(result).filter(function () { return this.MenuType == 1 }).each(function (k, v) {
+        if (v.Address == "" && v.Parent == "") {
+            var item = getParentMenu(v.Name, v.ID);
+            var menuItem = $(".nav-pills.nav-sidebar").append(item);
+        }
+    });
+    $("li.rootMenu").each(function (x, y) {
+        var rid = $(y).attr("tag");
+        var subMenu = $(result).filter(function () { return this.Parent == rid });
+
+        $(subMenu).each(function (x1, y1) {
+            $(y).children("ul.subMenu").append(getChildMenu(y1.Name, y1.Address));
+        });
+    });
+    
 }
